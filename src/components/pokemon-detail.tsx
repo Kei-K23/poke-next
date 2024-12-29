d5a727900a46c388e7989517b094247f1a15d750
Kei-K23 <keiksl2301@gmail.com>
2024-12-29 21:36:06 +0630

    feat: add responsive layout for pokemon detail page

--- src/components/pokemon-detail.tsx
+++ src/components/pokemon-detail.tsx
@@ -25,14 +25,14 @@ export default function PokemonDetail({ id }: PokemonDetailProps) {
         backgroundColor: getTypeColor(pokemon.types[0].type.name),
       }}
     >
-      <div className="container mx-auto px-4 py-8 flex gap-20 w-full">
+      <div className="container mx-auto px-4 py-8 flex flex-col lg:flex-row gap-20 w-full">
         <div className="text-white w-full space-y-8">
-          <h3 className="text-lg md:text-3xl lg:text-5xl capitalize font-bold">
+          <h3 className="text-xl md:text-3xl lg:text-5xl capitalize font-bold">
             #{pokemon.id.toString().padStart(4, "0")} <br /> {pokemon.name}
           </h3>
-          <div className="w-full flex justify-between">
+          <div className="w-full flex flex-col md:flex-row justify-between gap-6">
             <div>
-              <h3 className="text-xl md:text-2xl font-semibold mb-2">
+              <h3 className="text-base md:text-2xl font-semibold mb-2">
                 Abilities
               </h3>
               <div className="flex items-center gap-3">
@@ -47,7 +47,9 @@ export default function PokemonDetail({ id }: PokemonDetailProps) {
               </div>
             </div>
             <div>
-              <h3 className="text-xl md:text-2xl font-semibold mb-2">Types</h3>
+              <h3 className="text-base md:text-2xl font-semibold mb-2">
+                Types
+              </h3>
               <div className="flex items-center gap-3">
                 {pokemon.types.map((t) => (
                   <div
@@ -66,7 +68,7 @@ export default function PokemonDetail({ id }: PokemonDetailProps) {
           <div className="space-y-4">
             {pokemon.stats.map((stat, index) => (
               <div key={`${stat.stat}-${stat.base_stat}-${index}`}>
-                <div className="flex items-center gap-x-3 text-xl font-semibold mb-2">
+                <div className="flex items-center gap-x-3 text-base md:text-xl font-semibold mb-2">
                   <span className="capitalize">{stat.stat.name}</span>
                   <span>{stat.base_stat}</span>
                 </div>
@@ -74,25 +76,27 @@ export default function PokemonDetail({ id }: PokemonDetailProps) {
               </div>
             ))}
           </div>
-          <div className="text-black flex gap-4">
-            <div className="neo-brutalism-white p-4 font-semibold">
+          <div className="text-black flex flex-wrap gap-4">
+            <div className="neo-brutalism-white p-2 lg:p-4 font-semibold">
               <p className="flex items-center gap-x-1 mb-2">
                 <Ruler />
                 HEIGHT
               </p>
-              <span className="text-xl">{pokemon.height} M</span>
+              <span className="text-base lg:text-xl">{pokemon.height} M</span>
             </div>
-            <div className="neo-brutalism-white p-4 font-semibold">
+            <div className="neo-brutalism-white p-2 lg:p-4 font-semibold">
               <p className="flex items-center gap-x-1 mb-2">
                 <Weight /> WEIGHT
               </p>
-              <span className="text-xl">{pokemon.weight} KG</span>
+              <span className="text-base lg:text-xl">{pokemon.weight} KG</span>
             </div>
-            <div className="neo-brutalism-white p-4 font-semibold">
+            <div className="neo-brutalism-white p-2 lg:p-4 font-semibold">
               <p className="flex items-center gap-x-1 mb-2">
                 <Activity /> BASE XP
               </p>
-              <span className="text-xl">{pokemon.base_experience} XP</span>
+              <span className="text-base lg:text-xl">
+                {pokemon.base_experience} XP
+              </span>
             </div>
           </div>
         </div>
@@ -102,14 +106,14 @@ export default function PokemonDetail({ id }: PokemonDetailProps) {
             alt={pokemon?.name}
             width={400}
             height={400}
-            className="object-contain z-10 relative"
+            className="object-contain z-10 relative size-[200px] md:size-[300px] lg:size-[400px]"
           />
           <Image
             src={`/icons/${pokemon?.types[0].type.name}.svg`}
             alt={pokemon?.types[0].type.name || "pokemon type icon"}
-            width={700}
-            height={700}
-            className="object-contain absolute top-1/2 -translate-y-1/2"
+            width={600}
+            height={600}
+            className="object-contain absolute top-1/2 -translate-y-1/2 size-[350px] md:size-[400px] lg:size-[600px]"
           />
         </div>
       </div>
